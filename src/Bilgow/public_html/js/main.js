af24fb6bb2d43e493c168ed78619b5c509e61223
Gustavo Cordeiro de Oliveira <oliv.dg@gmail.com>
2013-10-15 14:40:29 -0300

    Adição da funcionalidade de hubs sociais

--- src/Bilgow/public_html/js/main.js
+++ src/Bilgow/public_html/js/main.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function(){
+        $('#btn-following').click(function(e){
+           $('#friends-hub').slideUp();
+           $('#following-hub').slideDown();
+        });
+        
+        $('#btn-friends').click(function(e){
+           $('#following-hub').slideUp();
+           $('#friends-hub').slideDown();
+        });
+});
